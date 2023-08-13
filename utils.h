76e58cdb1412d07832a5f5a0de6da7db5d2e525f
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-13 23:48:26 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -4,8 +4,7 @@
 #include "table.h"
 #include "globals.h"
 
+
 char *filename_suffix(char *str1, char *str2);
 
-char **split_string(char *str, char *separator, int *count);
-
-void free_split_string(char **splitString, int count);
+void skip_white_spaces(char currLine[]);
