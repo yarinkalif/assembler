ab5fb0c58f9f9319739e2fe1d08e3572837a13a8
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-13 23:49:47 +0300

    Update table.h

--- table.h
+++ table.h
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct macro_table_val {
+typedef struct macro {
 	char *macroName;
 	char *macroContent;
-	struct macro_table_val *next;
-} macro_table_val;
+	struct macro *next;
+} macro;
 
 
 typedef struct {
-	macro_table_val *head;
+	macro *head;
 } macro_table;
 
 
@@ -19,7 +19,7 @@ macro_table *create_macro_table();
 
 void add_macro_to_table(macro_table *table, char *name, char *content);
 
-char *get_macro_val(macro_table *table, char *macroName);
+char *expend_macro(char *line, macro_table *table);
 
 void free_macro_table(macro_table *table);
 
