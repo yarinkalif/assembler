2c8a82067148be1e16fc092b0b454b96efe75052
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-13 23:49:26 +0300

    Update table.c

--- table.c
+++ table.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "table.h"
+#include "globals.h"
 
 
 /*this function creats the linked list*/
 macro_table *create_macro_table() {
 	macro_table *table = (macro_table*)malloc(sizeof(macro_table));
-	if (table == NULL){
+	if (table == NULL){ /*check if the memory allocation successed*/
 		return NULL;
 	}
 	table->head = NULL;
@@ -15,36 +17,44 @@ macro_table *create_macro_table() {
 
 /*this function adds new macro to the table by using linked list*/
 void add_macro_to_table(macro_table *table, char *name, char *content) {
-	macro_table_val *newVal = (macro_table_val*) malloc(sizeof(macro_table_val)); /*create new space in the table for new macro*/
+	macro *newMacro = (macro*) malloc(sizeof(macro)); /*create new space in the table for new macro*/
 
-	newVal->macroName = (char*)malloc((strlen(name) + 1)*sizeof(char)); /*allocate memory for name*/
-	strcpy(newVal->macroName,name); /*copy the macro name*/
-
-	newVal->macroContent = (char*)malloc((strlen(content) + 1)*sizeof(char)); /*allocate memory for content*/
-	strcpy(newVal->macroContent,content); /*copy the macro content*/
-	
-	newVal->next = table->head; /*linked the new macro to the beginning of the list*/
-	table->head = newVal;
+	newMacro->macroName = (char*)malloc((strlen(name) + 1)*sizeof(char)); /*allocate memory for name*/
+	strcpy(newMacro->macroName,name); /*copy the macro name*/
+	newMacro->macroContent = (char*)malloc((strlen(content) + 1)*sizeof(char)); /*allocate memory for content*/
+	strcpy(newMacro->macroContent,content); /*copy the macro content*/
+	newMacro->next = table->head; /*linked the new macro to the beginning of the list*/
+	table->head = newMacro;
 }
 
-/*this function returns the macro value from the linked list*/
-char *get_macro_val(macro_table *table, char *macroName) {
-	macro_table_val *curr = table->head;
+/*this function replace macro name with his content*/
+char *expend_macro(char *line, macro_table *table) {
+	macro *curr = table->head; /*pointed to the head of the linked list*/
+	char *expendLine = (char *)malloc(MAX_LENGTH_LINE + 2);
 
-	while (curr != NULL) {
-		if (strcmp(curr->macroName, macroName) == 0) {
-			return curr->macroContent;
-		}
-		curr = curr->next; /*to the next value*/
+	if (expendLine == NULL) {
+		return NULL;
 	}
-	return NULL; /*if the macro not found*/
+	strcpy(expendLine, line);
+
+	while (curr != NULL){
+		char *macroPos = strstr(expendLine, curr->macroName); /*search for the macro name in the linked list*/
+		if (macroPos != NULL) { /*found the macro name*/
+			printf("22the line is: %s", expendLine);
+			strcpy(macroPos, curr->macroContent); /*replace the line with the macro content*/
+			return expendLine;
+		}
+		curr = curr->next;
+	}
+	return expendLine;
 }
+	
 
 /*this function free the memory used to the macro table*/
 void free_macro_table(macro_table *table) {
-	macro_table_val *curr = table->head;
+	macro *curr = table->head;
 	while (curr != NULL) {
-		macro_table_val *temp = curr;
+		macro *temp = curr;
 		curr = curr->next;
 		free(temp->macroName);
 		free(temp->macroContent);
