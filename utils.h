d05e733ed951efaa8c8462553ddc6f08ec84cdd2
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-22 14:10:14 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -5,6 +5,53 @@
 #include "globals.h"
 
 
+enum typeOfSentence {
+	EMPTY_LINE,
+	NOTE_LINE,
+	INSTRUCTION_LINE,
+	GUIDANCE_LINE
+};
+
+
 char *filename_suffix(char *str1, char *str2);
 
 void skip_white_spaces(char currLine[]);
+
+int type_of_sentence(char *currLine);
+
+char process_field(char* field);
+
+void handle_guidence(char* field, char* operands);
+
+void get_data_from_line(char *currLine, char *label, char *keyWord, char *operands, int numberLine);
+
+bool search_opcode_name(char *token, struct opcodeInfo *opcodeTable);
+
+int *get_opcode_code(char *opcodeName, struct opcodeInfo *opcodeTable);
+
+int symbol_exists(char* symbol_name);
+
+int get_instruction_type(char *keyWord, int *currOpcode);
+
+int operand_list_to_ascii(char *operands, int *list, int numberLine);
+
+int get_type_guidence(char *keyWord);
+
+void insert_symbol(char* symbol_name, int value, char type);
+
+int is_symbol(char* field);
+
+int is_alphanumeric(char* str);
+
+int symbol_exists(char* symbol_name);
+
+void ensure_address_in_bounds(int address);
+
+void memory_insert(int address, unsigned int value);
+
+void ensure_data_in_bounds(int data);
+
+void memory_insert_int(int address, int data);
+
+void memory_insert_char(int address, char ch);
+
