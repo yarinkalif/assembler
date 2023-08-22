a2548b157d033bd870d916d60fc356ca64ce6fa8
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-22 14:09:36 +0300

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -4,7 +4,7 @@
 
 int first_pass(char* fileName)
 {
-	int ic = IC;
+	int IC = IC;
 	int DC = DC;
 	int currNumberLine = 0;
 
@@ -20,15 +20,21 @@ int first_pass(char* fileName)
 	{
 		char *currLine = (char*)calloc()
 		if (!currLine) {
-			printf("error: alloctaion error of line);
-			
+			printf("error: alloctaion error of line\n");		
 	}
 
+	define_line(currLine);
 	
-}
+	fclose(source_file);
+	}/*end of  while (read_next_line(source_file, line))*/
+/	return 1;
+}/*end of void first_pass(FILE *source_file)*/ 
+
+
+
 
 /*this function define the line - empty, instruction, guidence or note*/
-int define_line() {
+int define_line(char *currLine) {
 	char currLine[MAX_LENGTH_LINE];
 	int currNumLine = 0;
 	char label, keyWord, operands;
@@ -41,26 +47,43 @@ int define_line() {
 	/*It is important to note that this operation repeats itself throughout the entire passage of the file, so we used a loop*/
 
 	while (fgets(currLine, MAX_LENGTH_LINE + 2, asFile) != NULL) {
+		char *field = strtok(line, " \t\n");
+		if (field == NULL) {
+			continue; // Empty or whitespace-only line, skip processing.
+		}
+		char guidence_type = process_field(field);
 		typeOfSentence = type_of_sentence(currLine); 
 		currNumLine++;
 		
+		/*check if the first field is label*/
+		if (label[0] != '\0') {
+			checkLabel = symbol_exists(label[0]);
+			if (checkLabel == 0) {
+				printf("Error: Symbol %s already exists in the symbol table.\n", label[0]);
+			}
+			else {
+				insert_symbol(label[0], IC, 'c');
+			}
+
+
 		if (typeOfSentence == INSTRUCTION_LINE || typeOfSentence == GUIDANCE_LINE) {
 			char tempCurrLine[MAX_LENGTH_LINE];
 			strcpy(tmpCurrLine, currLine);
 
-			get_data_from_line(tempCurrLine, label, keyWord, currNumLine);
-
-			if (label[0] != '\0') {
-				checkLabel = symbol_exists(label[0]);
-				if (checkLabel == 0) {
-					 printf("Error: Symbol %s already exists in the symbol table.\n", label[0]);
-				}
-			}
+			get_data_from_line(tempCurrLine, label, keyWord, currNumLine); /*check the type of the word*/
+		
 			if (typeOfSentence == INSTRUCTION_LINE) {
-				
+				return get_instruction_type(keyWord, tempCurrLine);
 			}
-
+			else if (typeOfSentence == GUIDANCE_LINE) {
+				return get_type_guidence(keyWord);
+			}
 		}
+		tempLineField = strtok(NULL, " \t\n"); /*move to the next token*/
+	}
+	return 0;
+	}/*end of while loop*/
+}
 
 
 /*this function get the data line*/
@@ -119,74 +142,32 @@ int line_data_image(data_image_ptr *currLine, data_image_ptr *currLinePtr, data_
 				switch (typeOfLine) {
 					case DATA:
 						tempLine->address = currAddress + countLine;
-						currAddress = currAddress + 12;
+						currAddress = currAddress + 1;
 						break;
 					case STRING:
-						
+						tempLine->address = currAddress + countLine;
+						break;
+				} /*end of switch*/
+				/*continue to the next line by pointer*/
+				if ((*currLinePtr) == NULL) {
+					(*currLinePtr) = tempLine
+					(*currLinePtr)->next = NULL;
+					(*tailPtr) = tempLine;
+				}
+				else {
+					(*tailPtr)->next = tempLine;
+					tempLine->next = NULL;
+					(*tailPtr) = tempLine;
+				}
+				countLine++;
+			} /*end of for loop*/
 
+			if (typeOfLine = DATA) {
+				countLine = countLine + NEXT_ADDRESS + 1;
+			} 
+			return countLine;
+		} /*end of switch*/
+	return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-		char tempLineField[MAX_LENGTH_LINE + 2];
-		strcpy(tempLineField, currLine);
-		char *field = strtok(tempLineField, " \t\n"); 
-		if (field == NULL) { 
-			continue; 
-		}
-
-		if (typeOfSentence == GUIDENCE_LINE)
-			{
-				char guidence_type = process_field(field);
-
-				if (guidence_type == 's') {
-					if (symbol_exists(field)) {
-						printf("Error: Symbol %s already exists in the symbol table.\n", field);
-					}
-					else {
-						insert_symbol(field, DC, SYMBOL_TYPE_DATA);
-						/* Continue processing the line to check for directives or commands following the symbol.*/
-						/*field = strtok(NULL, " \t\n");
-						instruction_type = process_field(field);
-					}
-				}
-
-				if (guidence_type == 'e') {
-					handle_instruction(field, strtok(NULL, ""));
-				}
-				else if (guidence_type == 'd') {  /* Handle data (.data or .string) here if needed.*/
-				/* Code for processing data directives can be added here.*/
-				/*}
-				else if (guidence_type == 'u') { /* Handle commands or unknown fields here.*/
-				/* Code for processing commands or handling errors for unknown fields can be added here.*/
-			/*	}
-			}
-		if (typeOfSentence == INSTRUCTION_LINE) {
-			
-
-		}
-		
-		tempLineField = strtok(NULL, " \t\n"); /*move to the next token*/
-
 	
-
-
-	/*fclose(source_file);
-	}/*end of  while (read_next_line(source_file, line))*/
-/*	return 1;
-}/*end of void first_pass(FILE *source_file)*/ 
-
-
-
