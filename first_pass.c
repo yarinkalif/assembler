c98e4c52af88b306c8c4acf5da85efbd36ed0462
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-22 17:41:59 +0300

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "first_pass.h"
 
+
 int first_pass(char* fileName)
 {
 	int IC = IC;
@@ -22,10 +23,8 @@ int first_pass(char* fileName)
 		if (!currLine) {
 			printf("error: alloctaion error of line\n");		
 	}
-
-	define_line(currLine);
 	
-	fclose(source_file);
+	/*fclose(source_file);*/
 	}/*end of  while (read_next_line(source_file, line))*/
 /	return 1;
 }/*end of void first_pass(FILE *source_file)*/ 
@@ -87,9 +86,10 @@ int define_line(char *currLine) {
 
 
 /*this function get the data line*/
-int line_data_image(data_image_ptr *currLine, data_image_ptr *currLinePtr, data_image_ptr *tailPtr, int typeOfSentence, long address, char *keyWord, char *operands, int numberLine) {
+int line_data_image(data_image_ptr *currLine, data_image_ptr *currLinePtr, data_image_ptr *tailPtr, int typeOfSentence, long address, char *keyWord, char *operands, int numberLine, long *ic, long *dc) {
 	int srcCodeflag = 1;
 	long currAddress = address;
+	int str_length = strlen(str);
 	int countLine = 0;
 
 	data_image_ptr currPtr = (data_image*) malloc(sizeof(data_image));
@@ -107,6 +107,7 @@ int line_data_image(data_image_ptr *currLine, data_image_ptr *currLinePtr, data_
 			strcpy(currPtr->src_code, currLine);
 			currPtr->address = address;
 			currPtr->next = NULL;
+			(*ic)++;
 
 			/**/
 			(*currLinePtr) = currPtr;
@@ -114,60 +115,79 @@ int line_data_image(data_image_ptr *currLine, data_image_ptr *currLinePtr, data_
 			break;
 		}
 		case GUIDENCE_LINE:
-		{
-			int *listOfOperands = (int*)malloc(MAX_LENGTH_LINE * sizeof(int));
-			if (!listOfOperands) {
-				printf("error allocation\n");
-	
-			int numOfOperands = operand_list_to_ascii(operands, listOfOperands, numberLine);
+		{	
+			if (get_type_guidence(keyWord) == 1) { /*if it's ".data"*/
+				int *listOfOperands = (int*)malloc(MAX_LENGTH_LINE * sizeof(int));
+				if (!listOfOperands) {
+					printf("Error: Memory allocation failed.\n");
+				}
+				int numOfOperands = operand_list_to_ascii(operands, listOfOperands, numberLine);
 
-			typeOfLine = get_type_guidence(keyWord);
-			if (typeOfLine == 0) {
-				printf("error: key word incorrect\n");
-			}
-			/*go over the operands*/
-			for (i=0; i<numOfOperands; i++) {
-				tempLine = (data_image*)malloc(sizeof(data_image);
-				if (!tempLine) {
-					printf("error allocation memory\n");
-				}
-				/*every operand added to the machine code*/
-				line_guidence_binary(tempLine->machine_code, keyWord, listOfOperands[i], currLine, typeOfLine, numberLine);
+				for (i = 0; i < numOfOperands; i++) {
+					currPtr = (data_image*)malloc(sizeof(data_image));
+					if (!currPtr) {
+						printf("Error: Memory allocation failed.\n");
+					}
+				
+					/*convert operand to machine code and update data image*/
+					line_guidence_binary(currPtr->machine_code, keyWord, listOfOperands[i], currLine, DATA, numberLine);
+			
+					/*update source code and data image properties*/
+					if (srcCodeflag == 1) {
+						strcpy(currPtr->src_code, currLine);
+						srcCodeflag = 0;
+					}
+					currPtr->address = *dc + countLine;
+					currPtr->next = NULL;
 
-				if (srcCodeflag == 1) { /*adding the source code*/
-					strcpy(tempLine->src_code, currLine);
-					srcCodeflag = 0;
-				}
-
-				switch (typeOfLine) {
-					case DATA:
-						tempLine->address = currAddress + countLine;
-						currAddress = currAddress + 1;
-						break;
-					case STRING:
-						tempLine->address = currAddress + countLine;
-						break;
-				} /*end of switch*/
-				/*continue to the next line by pointer*/
-				if ((*currLinePtr) == NULL) {
-					(*currLinePtr) = tempLine
-					(*currLinePtr)->next = NULL;
-					(*tailPtr) = tempLine;
-				}
-				else {
-					(*tailPtr)->next = tempLine;
-					tempLine->next = NULL;
-					(*tailPtr) = tempLine;
-				}
+					/*Update linked list of data images*/
+					if (*currLinePtr == NULL) {
+						*currLinePtr = currPtr;
+						(*currLinePtr)->next = NULL;
+						*tailPtr = currPtr;
+					} 
+					else {
+						(*tailPtr)->next = currPtr;
+						currPtr->next = NULL;
+						*tailPtr = currPtr;
+					}
+				(*dc)++;
 				countLine++;
-			} /*end of for loop*/
-
-			if (typeOfLine = DATA) {
-				countLine = countLine + NEXT_ADDRESS + 1;
-			} 
-			return countLine;
-		} /*end of switch*/
+				} /*end for loop*/
+				return countLine;
+			}
+			else if(get_type_guidence(keyWord) == 2) { /*if it's ".string"*/
+				for (i = 0; i<strLength; i++) {
+					data_image_ptr tempLine = (data_image_ptr)malloc(sizeof(data_image));	
+					if (!tempLine) {
+						printf("error: memory allocation\n");
+					}
+					char curr_char = (i < str_length) ? str[i] : '\0'; /*add the null terminator and update DC*/
+					/*encode the character and add to the machine code*/
+					line_guidence_binary(tempLine->machine_code, ".string", str[i], currLinePtr, STRING, 0);
+					/*update pointers and add source code and address*/
+					strcpy(tempLine->src_code, str);
+					tempLine->address = currAddress + (*dc);
+					if ((*currLinePtr) == NULL) {
+						(*currLinePtr) = tempLine;
+						(*currLinePtr)->next = NULL;
+						(*tailPtr) = tempLine;
+					} 
+					else {
+						(*tailPtr)->next = tempLine;
+						tempLine->next = NULL;
+						(*tailPtr) = tempLine;
+					}
+					(*dc)++;
+				} /*end of for loop*/
+			}
+			else if (get_type_guidence(keyWord) == 4) { /*if it's ".extern"*/
+				handle_instruction(field, strtok(NULL, ""))
+			}		
 	return 0;
 }
 
 	
+
+
+
