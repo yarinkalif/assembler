e99b8557e560176a8b9e703576d613e23bdff3b3
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-25 00:41:13 +0300

    Update table.h

--- table.h
+++ table.h
@@ -2,6 +2,10 @@
 #define _TABLE_H
 #include <stdio.h>
 #include <stdlib.h>
+#include "globals.h"
+#include "errors.h"
+
+#define TABLE_SIZE 80
 
 typedef struct macro {
 	char *macroName;
@@ -14,6 +18,32 @@ typedef struct {
 	macro *head;
 } macro_table;
 
+typedef struct s_table{
+	char **entries;
+} s_table;
+
+/* Define an entry in a symbols table */
+typedef struct s_table_entry{
+	char *symbol;
+	int symbol_addr;
+	symbol_type symbol_type;
+	struct s_table_entry *next;
+} s_table_entry;
+
+
+typedef struct {
+	char name[MAX_SYMBOL_LENGTH];
+	int value;
+	char type;
+	struct Symbol* next;
+} Symbol;
+
+typedef struct {
+    Symbol symbols[MAX_SYMBOL_LENGTH];
+    int count;
+} SymbolTable;
+
+SymbolTable symbol_table; 
 
 macro_table *create_macro_table();
 
@@ -22,5 +52,7 @@ void add_macro_to_table(macro_table *table, char *name, char *content);
 char *expend_macro(char *line, macro_table *table);
 
 void free_macro_table(macro_table *table);
+
+s_table *create_symbol_table();
 
 #endif
