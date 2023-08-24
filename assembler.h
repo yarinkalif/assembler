cd6f45b56433f28201d1a36e57070baa2fa3d60e
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-25 00:41:53 +0300

    Update assembler.h

--- assembler.h
+++ assembler.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*#include "first_pass.h"*/
+#include "preassembler.h"
+#include "table.h"
+#include "globals.h"
+#include "utils.h"
+#include "errors.h"
+
+
 int assembly_to_machine_code(char* fileName);
 
 #endif
