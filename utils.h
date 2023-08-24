4ac97750c51bf9cdd2a39bf9a3211fb4a960af34
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-25 00:41:37 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,16 +1,22 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
 #include "table.h"
 #include "globals.h"
-
+#include "errors.h"
 
 enum typeOfSentence {
-	EMPTY_LINE,
+	EMPTY_LINE = 0,
 	NOTE_LINE,
 	INSTRUCTION_LINE,
 	GUIDANCE_LINE
 };
+
 
 
 char *filename_suffix(char *str1, char *str2);
@@ -19,19 +25,19 @@ void skip_white_spaces(char currLine[]);
 
 int type_of_sentence(char *currLine);
 
-char process_field(char* field);
+char process_field(char* field, int IC);
 
 void handle_guidence(char* field, char* operands);
 
 void get_data_from_line(char *currLine, char *label, char *keyWord, char *operands, int numberLine);
 
-bool search_opcode_name(char *token, struct opcodeInfo *opcodeTable);
+/*bool search_opcode_name(char *token, struct opcodeInfo *opcodeTable1, struct opcodeInfo *opcodeTable2, struct opcodeInfo *opcodeTable3);*/
 
-int *get_opcode_code(char *opcodeName, struct opcodeInfo *opcodeTable);
+/*int get_opcode_code(char *opcodeName, struct opcodeInfo *opcodeTable);*/
 
-int symbol_exists(char* symbol_name);
+int symbol_exists(char* symbol_name, SymbolTable* current_table);
 
-int get_instruction_type(char *keyWord, int *currOpcode);
+/*int get_instruction_type(instruction_word *keyWord);*/
 
 int operand_list_to_ascii(char *operands, int *list, int numberLine);
 
@@ -43,9 +49,7 @@ int is_symbol(char* field);
 
 int is_alphanumeric(char* str);
 
-int symbol_exists(char* symbol_name);
-
-void ensure_address_in_bounds(int address);
+/*void ensure_address_in_bounds(int address);
 
 void memory_insert(int address, unsigned int value);
 
@@ -53,5 +57,6 @@ void ensure_data_in_bounds(int data);
 
 void memory_insert_int(int address, int data);
 
-void memory_insert_char(int address, char ch);
+void memory_insert_char(int address, char ch);*/
 
+#endif 
