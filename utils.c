db8c5c0c53a1f45a3dc84a07619d584d5a490137
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-26 23:06:49 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -89,41 +89,6 @@ int type_of_sentence(char *currLine) {
 	return INSTRUCTION_LINE;
 }
 
-
-/*The function receives a string and decides what type the field is - whether it is a symbol, a prompt or something else.*/
-/* It performs operations based on the type and returns a character that represents the type of the field*/
-/*char process_field(char* field, int IC) {
-    if (is_symbol(field)) {
-        insert_symbol(field, IC, 'c');
-        return 's';*/  /* symbol*/
-    /*} else if (strcmp(field, ".data") == 0 || strcmp(field, ".string") == 0) {
-        return 'd';*/  /* data_instruction*/
-    /*} else if (strcmp(field, ".extern") == 0 || strcmp(field, ".entry") == 0) {
-        return 'e';*/  /* external_entry_instruction */
-  /*  } else {
-        return 'u';*/  /* unknown*/
-   /* }
-}*/
-
-
-/*As we mentioned above, we used the algorithm in the course booklet.
- The following function provides an answer to the ninth step and checks whether it is an .extern directive*/
-/*void handle_guidence(char* field, char* operands) {
-    char guidence_type = process_field(field, IC);*/
-
-    /* If the directive is ".extern" */
-    /*if (guidence_type == 'e') {
-        char* token = strtok(operands, " \t,");
-        while (token != NULL) {*/
-            /* Add the symbol to the symbol table */
-           /* insert_symbol(token, 0, 'E');
-            token = strtok(NULL, " \t,");
-        }
-    }*/
-    /*TODO: Additional checks for other types of instructions can be added here if needed*/
-
-
-
 /*this function get the data from the line using tokens*/
 void get_data_from_line(char *currLine, char *label, char *keyWord, char *operands, int numberLine) {
 	int labelFlag = 0;
@@ -159,39 +124,6 @@ void get_data_from_line(char *currLine, char *label, char *keyWord, char *operan
 }
 
 
-/*this function search for the opcode name. using this to compare to token*/
-/*bool search_opcode_name(char *token, struct opcodeInfo *opcodeTable1, struct opcodeInfo *opcodeTable2, struct opcodeInfo *opcodeTable3) {
-	int i ;	
-	for (i = 0; i < MAX_OPCODES; i++) {
-		if (strcmp(token, opcodeTable1[i].name) == 0) {
-			return true;
-		}
-		if (strcmp(token, opcodeTable2[i].name) == 0) {
-			return true;
-		}
-		if (strcmp(token, opcodeTable3[i].name) == 0) {
-			return true;
-		}		
-	}
-	return false;*/ /*if the opcode is not find*/
-/*}*/
-
-
-/*this function return the hexadecimal opcode code*/
-/*int get_opcode_code(char *opcodeName, struct opcodeInfo *opcodeTable) {
-	int i ;
-	for (i = 0; i < sizeof(opcodeTable); i++) {
-		if (strcmp(opcodeName, opcodeTable[i].name) == 0) {
-			return opcodeTable[i].code;
-		}
-		else {
-			printf("error in the given opcode");
-		}	
-	}
-	return -1;*/ /*if the opcode is not find*/
-
-
-
 /*The following function checks if the symbol already exists in the table.*/
 int symbol_exists(char* symbol_name, SymbolTable* current_table) {
     int i;
@@ -212,7 +144,7 @@ int get_instruction_type(instruction_word *keyWord) {
 	int sizeOfKeyWordArray3 = 2;
 
 	for (i=0; i<sizeOfKeyWordArray1; i++) {
-		if (strcmp(keyWord, opcodeTable1[i].name) == 0) {
+		if (strcmp(keyWord->instruction_line.name, opcodeTable1[i].name) == 0) {
 			keyWord->instruction_line.ARE = 2;
 			keyWord->instruction_line.operandDst = 3;
 			keyWord->instruction_line.opcode = 4;
@@ -221,7 +153,7 @@ int get_instruction_type(instruction_word *keyWord) {
 		}
 	}
 	for (i=0; i<sizeOfKeyWordArray2; i++) {
-		if (strcmp(keyWord, opcodeTable2[i].name) == 0) {
+		if (strcmp(keyWord->instruction_line.name, opcodeTable2[i].name) == 0) {
 			keyWord->instruction_line.ARE = 2;
 			keyWord->instruction_line.operandDst = 3;
 			keyWord->instruction_line.opcode = 4;
@@ -230,7 +162,7 @@ int get_instruction_type(instruction_word *keyWord) {
 		}
 	}
 	for (i=0; i<sizeOfKeyWordArray3; i++) {
-		if (strcmp(keyWord, opcodeTable3[i].name) == 0) {
+		if (strcmp(keyWord->instruction_line.name, opcodeTable3[i].name) == 0) {
 			keyWord->instruction_line.ARE = 2;
 			keyWord->instruction_line.operandDst = 0;
 			keyWord->instruction_line.opcode = 4;
@@ -240,8 +172,6 @@ int get_instruction_type(instruction_word *keyWord) {
 	}
 	return 0;
 } /*if the key word didnt found*/
-
-
 
 
 /*this function represent the operands in ascii*/
@@ -341,32 +271,12 @@ Symbol *insert_symbol(char* symbol_name, int value, char type) {
 	strcpy(newSymbol->name, symbol_name);
 	newSymbol->value = value;
 	newSymbol->type = type;
-	/*symbol_table.symbols[symbol_table.count].value = value;
-	symbol_table.symbols[symbol_table.count].type = type;*/
 	symbol_table.count++; /*enlarging the table*/
 
 	return newSymbol;
 }
 
-
-/*The function checks whether the string passed to it is a symbol. It returns 1 (true) if the string is a symbol, and 0 (false) otherwise.*/
-int is_symbol(char* field) {
-    return ((field[0] >= 'A' && field[0] <= 'Z') || (field[0] >= 'a' && field[0] <= 'z')) && is_alphanumeric(field);
-}
-
-/*The function checks whether all the characters in the string passed to it are alphanumeric (letters or numbers).
- It returns 1 (true) if all characters are alphanumeric, and 0 (false) otherwise.*/
-int is_alphanumeric(char* str) {
-	int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9'))) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-
+/*this function create the files .externals and .entries*/
 void create_files (struct entry_symbols **head_entry, struct extern_symbols **head_extern, int create_entry_file, int create_extern_file) {
 	FILE *entry_ptr;
 	FILE *extern_ptr;
@@ -403,7 +313,8 @@ void create_files (struct entry_symbols **head_entry, struct extern_symbols **he
 		}
 	}
 }		
-		
+
+/*this function get a string and reverse it*/		
 void reverse_string(char *str) {
     int length = strlen(str), i;
     for (i = 0; i < length / 2; i++) {
@@ -413,46 +324,4 @@ void reverse_string(char *str) {
     }
 }
 
-/*The following function creates a limit on the address in memory*/
-/*void ensure_address_in_bounds(int address) {
-    if (address < 0 || address >= MEMORY_SIZE) {
-        handle_error("Address out of bounds");
-    }
-}*/
 
-
-/*The following function takes care of putting data into memory*/
-/*void memory_insert(int address, unsigned int value) {
-    ensure_address_in_bounds(address);*/
-    
-	/*Check that the value is not too big for memory*/	
-    /*if (value >= (1 << WORD_SIZE)) {
-        handle_error("Value too large for memory word");
-    }
-    memory[address] = value;
-}*/
-
-/*The following function delimits the size of the data that goes into memory*/
-/*void ensure_data_in_bounds(int data) {
-    if (data < -(1 << (WORD_SIZE - 1)) || data >= (1 << (WORD_SIZE - 1))) {
-        handle_error("Data out of bounds for word size");
-    }
-}*/
-
-/*The following function is a function for inserting a whole data (even a third) into memory using the 2's complement method*/
-/*void memory_insert_int(int address, int data) {
-    ensure_data_in_bounds(data);
-    ensure_address_in_bounds(address);*/
-
-/*    if (data < 0) {
-        memory[address] = (1 << WORD_SIZE) + data;*/ /*Converting to 2's complement*/
- /*   } else {
-        memory[address] = data;
-    }
-}*/
-
-/*function to insert a character into memory (when the character is ASCII)*/
-/*void memory_insert_char(int address, char ch) {
-    ensure_address_in_bounds(address);
-    memory[address] = ch;
-}*/
