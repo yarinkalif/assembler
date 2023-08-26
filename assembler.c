2b9f62c41ebc7ffdf0b37c524767aa7cc250c572
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-27 01:32:46 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -5,6 +5,8 @@
 #include "first_pass.h"
 #include "table.h"
 #include "errors.h"
+#include "utils.h"
+#include "globals.h"
 
 int main(int argc, char *argv[])
 {
@@ -30,12 +32,14 @@ return fileSuccess;
 /*this function calls to the preassembler, first past and second past functions*/
 int assembly_to_machine_code(char* fileName)
 {	long ic = 0, dc = 0;
-	int success = 0, line_number = 0;
+	int success = 0, line_number = 0, create_entry_file = 1, create_extern_file = 1;
 	char *currLine = (char*)calloc(MAX_LENGTH_LINE + 2, sizeof(char));
 	char *amFileName = (char*)malloc(256 * sizeof(char));
+	entry_symbols **head_entry;
+	extern_symbols **head_extern;
 
-	FILE *file_desc;
-	s_table *symbolTable = NULL;
+	FILE *file_dest;
+	SymbolTable *symbolTable = NULL;
 	
 
 	success = preassembler_file(fileName);
@@ -44,9 +48,9 @@ int assembly_to_machine_code(char* fileName)
 	}
 	
 	amFileName = filename_suffix(fileName, ".am");
-	file_desc = fopen(amFileName, "r");
-	if (file_desc == NULL) {
-		printf("couldn't open the file %s.\n", ".am");
+	file_dest = fopen(amFileName, "r");
+	if (file_dest == NULL) {
+		handle_error(ERROR_FILE_READ, line_number);
 		free(amFileName);
 		return 0;
 	}
@@ -54,8 +58,8 @@ int assembly_to_machine_code(char* fileName)
 	/*starting first pass*/
 	symbolTable = create_symbol_table(); /*we storage the symbols here*/
 
-	while (fgets(currLine, MAX_LENGTH_LINE + 2, file_desc) != NULL) { /*go over every line in the file*/
-		if (strchr(currLine, '\n') == NULL && !feof(file_desc)) {
+	while (fgets(currLine, MAX_LENGTH_LINE + 2, file_dest) != NULL) { /*go over every line in the file*/
+		if (strchr(currLine, '\n') == NULL && !feof(file_dest)) {
 			handle_error(ERROR_LINE_LENGTH, line_number); /*the length of line is too long*/
 			success = 0;
 		}
@@ -64,15 +68,25 @@ int assembly_to_machine_code(char* fileName)
 		}
 		else {
 			printf("the line is:%s\n", currLine);
-			success = first_pass(file_desc, currLine, symbolTable, &ic, &dc, line_number);
+			success = first_pass(file_dest, currLine, symbolTable, &ic, &dc, line_number);
 		}
 		line_number++;
 	}
 	
 	/*starting the second pass*/
-	/*rewind(file_desc);*/
+	rewind(file_dest);
+	if (success) {
+		success = second_pass(file_dest);
+	}
+
+	if (success) { /*print the files*/
+		create_files(head_entry, head_extern, create_entry_file, create_extern_file);
+	}
 			
-	free(amFileName);	
+	free(currLine);	
+	free(file_dest);
+	free(symbolTable);
+
 	
 	return success;
 }
