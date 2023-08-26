bbd85ee592d18492a09d4cad913dbb7f11846398
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2023-08-26 17:55:47 +0300

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "globals.h"
 #include "utils.h"
+#include "binary.h"
 
 
 /*this function represent the line instruction in binary*/
-void line_instruction_binary(char *machineCode, char *keyWord, char *operands, int numberLine) {
+void line_instruction_binary(char *machineCode, char *keyWord, int listOfInstruction, int numberLine) {
 	int currOpcode = 0;
-	instructionGroup1 currLine1;
-	instructionGroup2 currLine2;
-	instructionGroup3 currLine3;
+	instructionGroup currLine1;
+	instructionGroup currLine2;
+	instructionGroup currLine3;
 
 	int typeOfKeyWord = get_instruction_type(keyWord, &currOpcode);
 
@@ -47,7 +49,7 @@ OperandType identify_operand_type(char *operand) {
 }
 
 /*this function create the instruction of group 1 - the group with two operands*/
-instructionGroup1 create_instruction_1(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
+instruction_type create_instruction_1(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
 	instructionGroup1 instr;
 	instr->instruction.opcode = currOpcode;
 
@@ -94,7 +96,7 @@ instructionGroup1 create_instruction_1(char *keyword, char *operands, int currOp
 }	
 
 /*this function create the instruction of group 2 - the group with two operands*/
-instructionGroup2 create_instruction_2(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
+instruction_type create_instruction_2(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
 	instructionGroup1 instr;
 	instr->instruction.opcode = currOpcode;
 
@@ -114,7 +116,7 @@ instructionGroup2 create_instruction_2(char *keyword, char *operands, int currOp
 	instr.instruction.str = 0;	
 }
 
-instructionGroup3 create_instruction_3(char *keyword, int currOpcode, int typeOfKeyWord, int numberLine) {
+instruction_type create_instruction_3(char *keyword, int currOpcode, int typeOfKeyWord, int numberLine) {
 	instructionGroup1 instr;
 	instr->instruction.opcode = currOpcode;
 	instr.instruction.operandSrc = 0; /*the source operand is always 0*/
@@ -124,22 +126,16 @@ instructionGroup3 create_instruction_3(char *keyword, int currOpcode, int typeOf
 
 
 
-void line_guidence_binary(char *machineCode, char *keyWord, long dataList,int typeOfGuidence, int numberLine) {
+void line_guidence_binary(char *machineCode, char *keyWord, long dataList, int typeOfGuidence, int numberLine) {
 	DataCode currData; 
 	DataCode currStr;
 
-	switch(typeOfGuidence)
+	switch(typeOfGuidence) {
 		case DATA:
-			if (list > ....) {
-				printf("error\n");
-			}
 			currData.guidence.word = dataList;
 			int_to_binary(machineCode, currData.size, WORD);
 			break;
 		case STRING:
-			if (list > ....) {
-				printf("error\n");
-			}
 			currStr.guidence.word = currStr;
 			int_to_binary(machineCode, currStr.size, WORD);	
 			break;
@@ -152,6 +148,7 @@ void line_guidence_binary(char *machineCode, char *keyWord, long dataList,int ty
 /*this function convert integer to binary represantation*/
 void int_to_binary(char *machineCode, int curr, int numOfBit) {
 	unsigned mask = 1;
+	int numOfByte=0;
 
 	for (; mask; mask << 1) {
 		if (numOfByte != 0) {
@@ -163,14 +160,13 @@ void int_to_binary(char *machineCode, int curr, int numOfBit) {
 	}
 	strcat(machineCode, "\0"); /*Add the EOF marker to the end of the code*/
 	strrev(machineCode); /*change the order of bits to match the actual binary representation*/
-    }
 }
 
 static char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 
 char *base64_encode(unsigned char *input, size_t inputLength) {
-	size_t outputLength = 4*((inputLength+2)/3;
+	size_t outputLength = 4*((inputLength+2)/3);
 	char* encodedData = (char*)malloc(outputLength + 1);
 	if (encodedData == NULL) {
 		return NULL;
@@ -188,12 +184,12 @@ char *base64_encode(unsigned char *input, size_t inputLength) {
 		}
 
 		for (j=0; j<4; j++) {
-			encoded_data[i*4/3+j] = count > j ? base64_chars[(value >> (6*(3-j))) & 0x3F] : '=';
+			encodedData[i*4/3+j] = count > j ? base64_chars[(value >> (6*(3-j))) & 0x3F] : '=';
 		}
 	}
 
-	encoded_data[output_length] = '\0';
-	return encoded_data;
+	encodedData[outputLength] = '\0';
+	return encodedData;
 }
 
 char *encode_binary_word_to_base64(uint16_t binaryWord) {
@@ -203,4 +199,6 @@ char *encode_binary_word_to_base64(uint16_t binaryWord) {
 	bytes[1] = ((binaryWord & 0xF) << 4) & 0xF0;
 	return base64_encode(bytes,2);
 }
+
+
 
