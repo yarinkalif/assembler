d8108ff64ec3425f80046986b1e8005c118afb45
yarinkalif <128615850+yarinkalif@users.noreply.github.com>
2024-01-03 22:37:24 +0200

    Update assembler.h

--- assembler.h
+++ assembler.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*#include "first_pass.h"*/
 #include "preassembler.h"
 #include "table.h"
 #include "globals.h"
 #include "utils.h"
-#include "errors.h"
 
-
+/**
+ * Transforms the provided assembly file into machine code.
+ * Invokes functionalities from other included headers to achieve the transformation.
+ * @param fileName The name/path of the assembly file to process.
+ * @return Success status of assembly to machine code conversion.
+ */
 int assembly_to_machine_code(char* fileName);
 
 #endif
